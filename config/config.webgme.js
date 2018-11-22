8b28cf3c8a3f2dc901ca4d14a70903b3daa1247d
Gordon Stein <gstein@ltu.edu>
2018-11-22 15:06:52 -0800

    Add SVGs for some gates

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -9,10 +9,12 @@ var config = require('webgme/config/config.default'),
 // The paths can be loaded from the webgme-setup.json
 config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
 
+config.seedProjects.enable = true
+config.seedProjects.basePaths = ['seeds']
+config.visualization.svgDirs.push(__dirname + '/../assets/LogicGateSVGs/');
 
 
-
-
+config.seedProjects.defaultProject = 'LogicGates'
 
 
 // Visualizer descriptors
