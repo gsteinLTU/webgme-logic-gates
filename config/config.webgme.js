ad2d893ead69ad76cb7c1ec5e7a40536cae6bde8
Gordon Stein <gstein@ltu.edu>
2018-11-29 02:12:40 -0600

    Decorator Merge 2 (#2)

--- config/config.webgme.js
+++ config/config.webgme.js
@@ -10,10 +10,12 @@ var config = require('webgme/config/config.default'),
 config.plugin.basePaths.push(__dirname + '/../src/plugins');
 config.visualization.decoratorPaths.push(__dirname + '/../src/decorators');
 
+config.seedProjects.enable = true
+config.seedProjects.basePaths = ['seeds']
+config.visualization.svgDirs.push(__dirname + '/../assets/LogicGateSVGs/');
 
 
-
-
+config.seedProjects.defaultProject = 'LogicGates'
 
 
 // Visualizer descriptors
