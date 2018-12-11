9037f925fdb94fecbc9e7123fe6d113ca4d1832a
Gordon Stein <gstein@ltu.edu>
2018-12-11 16:42:04 -0600

    Testing

--- src/visualizers/widgets/CircuitSimulator/CircuitSimulatorWidget.js
+++ src/visualizers/widgets/CircuitSimulator/CircuitSimulatorWidget.js
@@ -29,15 +29,20 @@ define(['css!./styles/CircuitSimulatorWidget.css'], function () {
         // set widget class
         this._el.addClass(WIDGET_CLASS);
 
-        // Create a dummy header 
-        this._el.append('<iframe src="/routers/DigitalJSRouter/get" style="width:100%;height:100%; padding: 0; margin: 0;"></iframe>');
+        // Load simulator in iframe
+        this._el.append('<iframe id="simframe" src="/routers/DigitalJSRouter/get" style="width:100%;height:100%; padding: 0; margin: 0;"></iframe>');
 
-        // Registering to events can be done with jQuery (as normal)
-        this._el.on('dblclick', function (event) {
-            event.stopPropagation();
-            event.preventDefault();
-            self.onBackgroundDblClick();
-        });
+        var sendmessage = () => {
+            console.log('hi');
+            // Send file to iframe
+            if($('#simframe')[0] != undefined){
+                $('#simframe')[0].contentWindow.postMessage("hi");
+            } else {
+                setTimeout(sendmessage, 500);
+            }
+        }
+        
+        sendmessage();
     };
 
     CircuitSimulatorWidget.prototype.onWidgetContainerResize = function (width, height) {
@@ -46,35 +51,12 @@ define(['css!./styles/CircuitSimulatorWidget.css'], function () {
 
     // Adding/Removing/Updating items
     CircuitSimulatorWidget.prototype.addNode = function (desc) {
-        // if (desc) {
-        //     // Add node to a table of nodes
-        //     var node = document.createElement('div'),
-        //         label = 'children';
-
-        //     if (desc.childrenIds.length === 1) {
-        //         label = 'child';
-        //     }
-
-        //     this.nodes[desc.id] = desc;
-        //     node.innerHTML = 'Adding node "' + desc.name + '" (click to view). It has ' + 
-        //         desc.childrenIds.length + ' ' + label + '.';
-
-        //     this._el.append(node);
-        //     node.onclick = this.onNodeClick.bind(this, desc.id);
-        // }
     };
 
     CircuitSimulatorWidget.prototype.removeNode = function (gmeId) {
-        // var desc = this.nodes[gmeId];
-        // this._el.append('<div>Removing node "' + desc.name + '"</div>');
-        // delete this.nodes[gmeId];
     };
 
     CircuitSimulatorWidget.prototype.updateNode = function (desc) {
-        // if (desc) {
-        //     this._logger.debug('Updating node:', desc);
-        //     this._el.append('<div>Updating node "' + desc.name + '"</div>');
-        // }
     };
 
     /* * * * * * * * Visualizer event handlers * * * * * * * */
